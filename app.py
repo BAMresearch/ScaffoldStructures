b1aaa737f477dfbf7644bce06968bb0cdd9be9fd
Sven F <25983258+Spanholz@users.noreply.github.com>
2020-01-10 11:39:17 +0100

    Updated license in app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
+# Copyright (C) 2015 Jairson Dinis 
+# Copyright (C) 2018, 2019 Sven Fritzsche 
+# 
+#
+#
 # -*- coding: utf-8*-
+#This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
 
-"""
-This program creates 3D-dimensional low surfaces structures and allows
-the export into the .stl-format.
-"""
+#
+# This program creates 3D-dimensional low surfaces structures and allows
+# the export into the .stl-format.
+#
 
 
 import sys
